cb363a766a351fc93635a75e1feb6c8be0a3d176
krebstock <kyle.rebstock@eleadcrm.com>
2018-06-01 08:51:18 -0500

    added services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,20 @@ import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
 import { OrdersComponent } from './orders/orders.component';
 import { SearchBoxDirective } from './search-box.directive';
+import { SalesDataService } from './sales-data.service';
+import { SalesData2Service } from './sales-data2.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerComponent,
     OrdersComponent,
-    SearchBoxDirective
+    SearchBoxDirective,
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [SalesDataService, SalesData2Service],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
