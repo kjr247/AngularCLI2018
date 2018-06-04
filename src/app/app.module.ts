d31c3f72964cd8d1422dae7033e2a47a75d71b59
krebstock <kyle.rebstock@eleadcrm.com>
2018-06-04 08:50:03 -0500

    add to app.module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
@@ -8,6 +10,7 @@ import { SearchBoxDirective } from './search-box.directive';
 import { SalesDataService } from './sales-data.service';
 import { SalesData2Service } from './sales-data2.service';
 import { InitCapsPipe } from './shared/init-caps.pipe';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { InitCapsPipe } from './shared/init-caps.pipe';
     InitCapsPipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    LoginModule
   ],
   providers: [SalesDataService, SalesData2Service],
   bootstrap: [AppComponent]
